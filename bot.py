ea7b9312e5b0169d586b334630d53b480b5ba2c8
amoma291 <amoma291@users.noreply.github.com>
2025-04-02 13:14:31 +0300

    fixed gitignore

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
-import config
-import handlers
+import os
+
+from dotenv import load_dotenv
 from telegram.ext import (Updater, CallbackQueryHandler, CommandHandler,
                           ConversationHandler, MessageHandler, Filters)
 
+import handlers
+
+load_dotenv()
+
 
 def main():
-    bot = Updater(config.API_TOKEN)
+    bot = Updater(os.environ.get('API_TOKEN'), use_context=True)
     dp = bot.dispatcher
     dp.add_handler(CommandHandler('start', handlers.greet_user))
     dp.add_handler(CallbackQueryHandler(handlers.publish_post))
